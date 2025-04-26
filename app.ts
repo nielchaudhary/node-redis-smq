7a21680ceea4fa579bd16e1d77de29eddf9bd743
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-04-27 01:59:35 +0530

    removed unused code

--- app.ts
+++ app.ts
@@ -3,11 +3,8 @@ import { queueOps } from "./queue";
 
 const app = express();
 
-console.log("before queue");
-
 queueOps();
 
-console.log("after queue");
 app.listen(3000, () => {
   console.log("SERVER ON 3k");
 });
